00da05a4f4a1f355c3ac3eb119c11da455cf7c8a
Kevin Tu <kevin.b.tu@gmail.com>
2014-08-13 14:43:09 -0700

    made homepage responsive with overlay

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,21 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function randomOverlay(){
+	console.log('find a random overlay');
+	var newColor = getRandomColor();
+	var p = $("#background-overlay").css("background-color", newColor);
+	// p.hide(100).show(100);
+	console.log(newColor);
+}
+
+
 $( "#home-content-container" ).hover(
   function() {
     $("#home-inner-content").html("");
@@ -7,3 +25,17 @@ $( "#home-content-container" ).hover(
     $('#home-inner-content').children("div").remove();
   }
 );
+
+$( "#home-content-container" ).hover(function(){
+	randomOverlay()
+});
+
+
+
+// setInterval(randomOverlay, 2500);
+
+// var p = $("#background-overlay").css("background-color", "yellow");
+// p.hide(1500).show(1500);
+// p.queue(function() {
+//   p.css("background-color", "red");
+// });
